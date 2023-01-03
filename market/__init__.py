9fcb35cdf456c263150034a539e75a91c3b7c818
Mouhib-Khemiri <khemiri.mouhib43@gmail.com>
2023-01-03 21:42:16 +0100

    Modified

--- market/__init__.py
+++ market/__init__.py
@@ -9,4 +9,6 @@ app.config['SECRET_KEY']='61b10de0d0196999a7960657'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
 from market import routes
